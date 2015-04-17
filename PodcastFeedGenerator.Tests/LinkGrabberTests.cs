1994044816180180b5d2afca0487b80fb14b7b8a
Tomasz Pamula <tomasz.p.pamula@gmail.com>
2015-04-17 23:38:51 +0200

    Grabs feed links and their descriptions from file.

---
+++ PodcastFeedGenerator.Tests/LinkGrabberTests.cs
@@ -0,0 +1,28 @@
+﻿using ApprovalTests;
+using ApprovalTests.Namers;
+using ApprovalTests.Reporters;
+using PodcastFeedGenerator.Models;
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace PodcastFeedGenerator.Tests
+{
+    [UseReporter(typeof(DiffReporter))]
+    [UseApprovalSubdirectory("Approvals")]
+    public class LinkGrabberTests
+    {
+        private readonly string _testFilePath = @"..\..\Data\IE-2015-04-17.html";
+
+        [Fact]
+        private void should_grab_links()
+        {
+            var input = File.ReadAllText(_testFilePath);
+
+            var linkGrabber = new LinkGrabber();
+            var result = linkGrabber.Grab(input);
+
+            Approvals.VerifyAll(result, "[name, link]");
+        }
+    }
+}
